a7f121977d7b9900750e8dbd5185b753b6099100
Justinio14 <jbutwell@hotmail.com>
2017-03-09 11:17:28 +0000

    added withdrawl method and tests

--- lib/account.rb
+++ lib/account.rb
@@ -1,10 +1,18 @@
 class Account
 
-  def balance
+attr_reader :balance
 
+  def initialize()
+    @balance = 0
   end
 
   def deposit(credit)
+    @credit = credit
+    @balance += @credit
+  end
 
+  def withdrawl(debit)
+    @debit = debit
+    @balance -= @debit
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,11 +8,32 @@ describe Account do
     it "shows balance" do
       expect(account).to respond_to(:balance)
     end
+
+    it "shows a balance of 0" do
+      expect(account.balance).to eq(0)
+    end
   end
+
+
 
   describe "deposit money" do
     it "able to add money" do
       expect(account).to respond_to(:deposit).with(1).argument
+    end
+
+    it "a 1000 deposit increases balance by 1000" do
+      expect{account.deposit(1000)}.to change{account.balance}.by(1000)
+    end
+  end
+
+  describe "deposit money" do
+    it "able to withdrawl money" do
+      expect(account).to respond_to(:withdrawl).with(1).argument
+    end
+
+    it "a 500 withdrawl decreases balance by 500" do
+      account.deposit(100)
+      expect{account.withdrawl(500)}.to change{account.balance}.by(-500)
     end
   end
 
